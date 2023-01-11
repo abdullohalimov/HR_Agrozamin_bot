cd8429c705fee8658eed750c5bf10a4d2318a6f9
Abdulloh <abdulloxcobra@gmail.com>
2023-01-11 18:58:38 +0500

    init

--- tgbot/handlers/test.py
+++ tgbot/handlers/test.py
@@ -13,16 +13,20 @@ async def test_start(message: Message, state: FSMContext):
     user_lang = data.get('language')
     questions = get_questions(lang=user_lang, category=data.get('prog_lang_id'))
     await state.update_data(questions=questions)
-    if data.get('extra_category') == [] or None:
-        await message.answer("Testlardan o'tish uchun 30 daqiqa vaqt beriladi. Testlardagi savollar {prog_lang} bo'yicha bo'ladi".format(prog_lang=data.get('prog_lang')), reply_markup=start_test_inl_kb(user_lang))
-    else:
+    # await state.update_data(q_count=len(questions.keys()))
+    
+    try:
         await message.answer("Testlardan o'tish uchun 30 daqiqa vaqt beriladi. Testlardagi savollar {extra_category} va {prog_lang} bo'yicha bo'ladi".format(prog_lang=data.get('prog_lang'), extra_category=", ".join(data.get('extra_category'))), reply_markup=start_test_inl_kb(user_lang))
+    except Exception:
+        await message.answer("Testlardan o'tish uchun 30 daqiqa vaqt beriladi. Testlardagi savollar {prog_lang} bo'yicha bo'ladi".format(prog_lang=data.get('prog_lang')), reply_markup=start_test_inl_kb(user_lang))
 
     await CategoryTests.start.set()
 
 async def questions_callbacks(c: CallbackQuery, state: FSMContext, callback_data: dict):
     statee = await state.get_state()
     data = await state.get_data()
+    questions = data.get('questions')
+    question = questions.pop(list(questions.ke))
     answers = data.get('answers', dict(questions={}))
     if statee == "CategoryTests:start":
         question = data.get('questions')[0]
@@ -35,123 +39,7 @@ async def questions_callbacks(c: CallbackQuery, state: FSMContext, callback_data
         await c.message.edit_text(f"2: {question['question']}", reply_markup=test_question_inl_kb(qid=question['id'], A=question["A"], B=question["B"], C=question["C"], D=question["D"], category=question['category']))
         await CategoryTests.next()
 
-    elif statee == "CategoryTests:question2":
-        answers['questions'][callback_data.get('id')] = callback_data.get('choice')
-        
-        question = data.get('questions')[2]
-        await c.message.edit_text(f"3: {question['question']}", reply_markup=test_question_inl_kb(qid=question['id'], A=question["A"], B=question["B"], C=question["C"], D=question["D"], category=question['category']))
-        await CategoryTests.next()
-
-    elif statee == "CategoryTests:question3":
-        answers['questions'][callback_data.get('id')] = callback_data.get('choice')
-        
-        question = data.get('questions')[3]
-        await c.message.edit_text(f"4: {question['question']}", reply_markup=test_question_inl_kb(qid=question['id'], A=question["A"], B=question["B"], C=question["C"], D=question["D"], category=question['category']))
-        await CategoryTests.next()
-
-    elif statee == "CategoryTests:question4":
-        answers['questions'][callback_data.get('id')] = callback_data.get('choice')
-        
-        question = data.get('questions')[4]
-        await c.message.edit_text(f"5: {question['question']}", reply_markup=test_question_inl_kb(qid=question['id'], A=question["A"], B=question["B"], C=question["C"], D=question["D"], category=question['category']))
-        await CategoryTests.next()
-
-    elif statee == "CategoryTests:question5":
-        answers['questions'][callback_data.get('id')] = callback_data.get('choice')
-        question = data.get('questions')[5]
-        await c.message.edit_text(f"6: {question['question']}", reply_markup=test_question_inl_kb(qid=question['id'], A=question["A"], B=question["B"], C=question["C"], D=question["D"], category=question['category']))
-        await CategoryTests.next()
-
-    elif statee == "CategoryTests:question6":
-        answers['questions'][callback_data.get('id')] = callback_data.get('choice')
-        question = data.get('questions')[6]
-        await c.message.edit_text(f"7: {question['question']}", reply_markup=test_question_inl_kb(qid=question['id'], A=question["A"], B=question["B"], C=question["C"], D=question["D"], category=question['category']))
-        await CategoryTests.next()
-
-    elif statee == "CategoryTests:question7":
-        answers['questions'][callback_data.get('id')] = callback_data.get('choice')
-        question = data.get('questions')[7]
-        await c.message.edit_text(f"8: {question['question']}", reply_markup=test_question_inl_kb(qid=question['id'], A=question["A"], B=question["B"], C=question["C"], D=question["D"], category=question['category']))
-        await CategoryTests.next()
-
-    elif statee == "CategoryTests:question8":
-        answers['questions'][callback_data.get('id')] = callback_data.get('choice')
-        question = data.get('questions')[8]
-        await c.message.edit_text(f"9: {question['question']}", reply_markup=test_question_inl_kb(qid=question['id'], A=question["A"], B=question["B"], C=question["C"], D=question["D"], category=question['category']))
-        await CategoryTests.next()
-
-    elif statee == "CategoryTests:question9":
-        answers['questions'][callback_data.get('id')] = callback_data.get('choice')
-        question = data.get('questions')[9]
-        await c.message.edit_text(f"10: {question['question']}", reply_markup=test_question_inl_kb(qid=question['id'], A=question["A"], B=question["B"], C=question["C"], D=question["D"], category=question['category']))
-        await CategoryTests.next()
-
-    elif statee == "CategoryTests:question10":
-        answers['questions'][callback_data.get('id')] = callback_data.get('choice')
-        question = data.get('questions')[10]
-        await c.message.edit_text(f"11: {question['question']}", reply_markup=test_question_inl_kb(qid=question['id'], A=question["A"], B=question["B"], C=question["C"], D=question["D"], category=question['category']))
-        await CategoryTests.next()
-
-    elif statee == "CategoryTests:question11":
-        answers['questions'][callback_data.get('id')] = callback_data.get('choice')
-        question = data.get('questions')[1]
-        await c.message.edit_text(f"12: {question['question']}", reply_markup=test_question_inl_kb(qid=question['id'], A=question["A"], B=question["B"], C=question["C"], D=question["D"], category=question['category']))
-        await CategoryTests.next()
-
-    elif statee == "CategoryTests:question12":
-        answers['questions'][callback_data.get('id')] = callback_data.get('choice')
-        question = data.get('questions')[12]
-        await c.message.edit_text(f"13: {question['question']}", reply_markup=test_question_inl_kb(qid=question['id'], A=question["A"], B=question["B"], C=question["C"], D=question["D"], category=question['category']))
-        await CategoryTests.next()
-
-    elif statee == "CategoryTests:question13":
-        answers['questions'][callback_data.get('id')] = callback_data.get('choice')
-        question = data.get('questions')[13]
-        await c.message.edit_text(f"14: {question['question']}", reply_markup=test_question_inl_kb(qid=question['id'], A=question["A"], B=question["B"], C=question["C"], D=question["D"], category=question['category']))
-        await CategoryTests.next()
-        answers['questions'][callback_data.get('id')] = callback_data.get('choice')
-
-    elif statee == "CategoryTests:question14":
-        answers['questions'][callback_data.get('id')] = callback_data.get('choice')
-        question = data.get('questions')[14]
-        await c.message.edit_text(f"15: {question['question']}", reply_markup=test_question_inl_kb(qid=question['id'], A=question["A"], B=question["B"], C=question["C"], D=question["D"], category=question['category']))
-        await CategoryTests.next()
-
-    elif statee == "CategoryTests:question15":
-        answers['questions'][callback_data.get('id')] = callback_data.get('choice')
-        question = data.get('questions')[15]
-        await c.message.edit_text(f"16: {question['question']}", reply_markup=test_question_inl_kb(qid=question['id'], A=question["A"], B=question["B"], C=question["C"], D=question["D"], category=question['category']))
-        await CategoryTests.next()
-
-    elif statee == "CategoryTests:question16":
-        answers['questions'][callback_data.get('id')] = callback_data.get('choice')
-        question = data.get('questions')[16]
-        await c.message.edit_text(f"17: {question['question']}", reply_markup=test_question_inl_kb(qid=question['id'], A=question["A"], B=question["B"], C=question["C"], D=question["D"], category=question['category']))
-        await CategoryTests.next()
-
-    elif statee == "CategoryTests:question17":
-        answers['questions'][callback_data.get('id')] = callback_data.get('choice')
-        question = data.get('questions')[17]
-        await c.message.edit_text(f"18: {question['question']}", reply_markup=test_question_inl_kb(qid=question['id'], A=question["A"], B=question["B"], C=question["C"], D=question["D"], category=question['category']))
-        await CategoryTests.next()
-
-    elif statee == "CategoryTests:question18":
-        answers['questions'][callback_data.get('id')] = callback_data.get('choice')
-        question = data.get('questions')[18]
-        await c.message.edit_text(f"19: {question['question']}", reply_markup=test_question_inl_kb(qid=question['id'], A=question["A"], B=question["B"], C=question["C"], D=question["D"], category=question['category']))
-        await CategoryTests.next()
-
-    elif statee == "CategoryTests:question19":
-        answers['questions'][callback_data.get('id')] = callback_data.get('choice')
-        question = data.get('questions')[19]
-        await c.message.edit_text(f"20: {question['question']}", reply_markup=test_question_inl_kb(qid=question['id'], A=question["A"], B=question["B"], C=question["C"], D=question["D"], category=question['category']))
-        await CategoryTests.next()
-
-    elif statee == "CategoryTests:question20":
-        answers['questions'][callback_data.get('id')] = callback_data.get('choice')
-        # question = data.get('questions')[20]
-        # await c.message.edit_text(f"1: {question['question']}", reply_markup=test_question_inl_kb(qid=question['id'], A=question["A"], B=question["B"], C=question["C"], D=question["D"], category=question['category']))
-        # await CategoryTests.next()
+    
         
     await state.update_data(answers=answers)
     print(answers)
--- tgbot/services/api.py
+++ tgbot/services/api.py
@@ -31,26 +31,7 @@ async def extra_categories(lang, sess: aiohttp.ClientSession):
             print(ret)
             return ret
 
-# async def register_user(chat_id, full_name, phone_number, gender, education, age, progra_language, extra_skills, resume_name, lang):
-#     if lang == "de":
-#         lang = 'uz'
-#     payload={'chat_id': chat_id,
-#     'full_name': full_name,
-#     'phone_number': phone_number,
-#     'gender': gender,
-#     'education': education,
-#     'age': age,
-#     'program_language': progra_language,
-#     'extra_skill': extra_skills,
-#     'cv': open(resume_name, 'rb') }
-#     # files=[
-#     # ('cv',(resume_name,open(resume_name,'rb'),'application/json'))]
-#     async with aiohttp.ClientSession() as session:
-#         async with session.post(f'http://139.162.159.187:8000/{lang}/register/', data=payload) as resp:
-#             resp = await resp.text()
 
-#             print(resp)
-#             return resp
 
 def register(chat_id, full_name, phone_number, gender, education, age, progra_language, extra_skills, resume_name, lang):
     url = f"http://139.162.159.187:8000/{lang}/register/"
@@ -72,23 +53,58 @@ def register(chat_id, full_name, phone_number, gender, education, age, progra_la
 
     return response.text
 
-# asyncio.run(register_user(lang='ru', chat_id='123456',progra_language='1', full_name='Abdulloh a a', phone_number="998888888", gender='E', education="Oliy", age="18-24", extra_skills=['1', '2'], resume_name='1357813137 resume HR-agrozamin.postman_collection.json'))
-# print(register(lang='ru', chat_id='123456',progra_language='1', full_name='Abdulloh a a', phone_number="998888888", gender='E', education="Oliy", age="18-24", extra_skills=['1', '2'], resume_name='1357813137 resume 1357813137_998998881965.docx'))
 
 def get_questions(lang, category):
     url = f"http://139.162.159.187:8000/{lang}/question/?category_id={category}"
 
     response = requests.request("GET", url)
     resp = response.json()
+    resp
+    dictt = {}
+    for i in range(1, len(resp) + 1):
 
-    return resp
-    # for res in resp:
-    #     print(res['id'])
-    #     print(res['question'])
-    #     print(res['A'])
-    #     print(res['B'])
-    #     print(res['C'])
-    #     print(res['D'])
-    #     print(res['category'])
+        dictt[i] = resp[i-1]
+
+    return dictt
+
+
+print(get_questions('uz', '2'))
+
+
+
+
+# for res in resp:
+#     print(res['id'])
+#     print(res['question'])
+#     print(res['A'])
+#     print(res['B'])
+#     print(res['C'])
+#     print(res['D'])
+#     print(res['category'])
 
 # get_questions()
+
+# async def register_user(chat_id, full_name, phone_number, gender, education, age, progra_language, extra_skills, resume_name, lang):
+#     if lang == "de":
+#         lang = 'uz'
+#     payload={'chat_id': chat_id,
+#     'full_name': full_name,
+#     'phone_number': phone_number,
+#     'gender': gender,
+#     'education': education,
+#     'age': age,
+#     'program_language': progra_language,
+#     'extra_skill': extra_skills,
+#     'cv': open(resume_name, 'rb') }
+#     # files=[
+#     # ('cv',(resume_name,open(resume_name,'rb'),'application/json'))]
+#     async with aiohttp.ClientSession() as session:
+#         async with session.post(f'http://139.162.159.187:8000/{lang}/register/', data=payload) as resp:
+#             resp = await resp.text()
+
+#             print(resp)
+#             return resp
+
+
+# asyncio.run(register_user(lang='ru', chat_id='123456',progra_language='1', full_name='Abdulloh a a', phone_number="998888888", gender='E', education="Oliy", age="18-24", extra_skills=['1', '2'], resume_name='1357813137 resume HR-agrozamin.postman_collection.json'))
+# print(register(lang='ru', chat_id='123456',progra_language='1', full_name='Abdulloh a a', phone_number="998888888", gender='E', education="Oliy", age="18-24", extra_skills=['1', '2'], resume_name='1357813137 resume 1357813137_998998881965.docx'))
